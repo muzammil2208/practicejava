747fa9b8028609a1460caec45301668485cb525b
Muzammil Khan <muzammilsk90@gmail.com>
2020-12-10 12:42:32 +0530

    binary search treee

--- binarysearchtree.java
+++ binarysearchtree.java
@@ -2,7 +2,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 
-
 public class binarysearchtree {
     static Node root;
     public void insert(Node root,int key)
@@ -61,46 +60,139 @@ public class binarysearchtree {
             }
         }
     }
-   public void delete(int key)
-   {
-       if(root==null)
-       {
-           System.out.println("tree is empty");
-       }
-       else
-       {
+    public void delete(int key)
+    {
+        //searching algorithm
+        Node deletenode;
+        deletenode=null;
         Node temp=root;
-           while(true)
-           {
-            
-            if(key<temp.data)
+        while(temp!=null)
+        {
+           
+            if(temp.data==key)
+            {
+                deletenode=temp;
+                break;
+            }
+            else if(key<temp.data)
             {
                 if(temp.left.data==key)
                 {
-                    temp.left.left.right=temp.left.right;
-                    temp.left=temp.left.left;
+                    deletenode=temp.left;
                     break;
                 }
                 else
-                temp=temp.left;   
+                temp=temp.left;
             }
             else if(key>temp.data)
             {
                 if(temp.right.data==key)
                 {
-                    temp.right.right.right=temp.right.right;
-                    temp.right=temp.right.right;
+                    deletenode=temp.right;
                     break;
                 }
                 else
                 temp=temp.right;
             }
+            
+        }
+        //deletion logic
+        //case 1 where node might not exist
+        if(deletenode==null)
+        {
+            System.out.println("The node node found inside the tree");
+        }
+        //case 2 where node might be the leaf node
+        if(deletenode.left==null&&deletenode.right==null)
+        {
+            if(key<temp.data)
+            {
+                temp.left=null;
+            }
+            else if(key>temp.data)
+            {
+                temp.right=null;
+            }
+         }
+         //case 3 where node might contain only one node either left or right
+        else if(deletenode.left==null||deletenode.right==null)
+        {
+            if(deletenode.left!=null)
+            temp.left=deletenode.left;
+            else if(deletenode.right!=null)
+            temp.right=deletenode.right;
+        }
+        //case 4 where node might contain both left and right child nodes
+        else if(deletenode.left!=null&&deletenode.right!=null)
+        {
+           Node min=findminimum(deletenode);
            
-           }
-          
-       }
+           delete(min.data);
+           deletenode.data=min.data;
+            
+        }
+
+
+
+    }
+    public static Node findminimum(Node root)
+    {
+        
+        Node temp=root.right;
+        //becase finding minimum from the right subtree for deletiong
        
-   } 
+        while(temp.left!=null)
+        {
+            temp=temp.left;
+        }
+        return temp;
+
+    }
+    public void minimummaximum(Node root)
+    {
+        int min=root.data;
+        int max=root.data;
+        Node temp=root;
+        while(temp.left!=null)
+        {
+            
+            temp=temp.left;
+            min=temp.data;
+        }
+        temp=root;
+        while(temp.right!=null)
+        {
+            
+            temp=temp.right;
+            max=temp.data;
+        }
+        System.out.println("Maximum in  tree is"+max);
+        System.out.println("Minimum in tree is"+min);
+    }
+   
+   public void findarray(Node root)
+   {
+       if(root==null)
+       return;
+       int a[]=new int[100];
+       findallpaths(root,a,0);
+   }
+    public void findallpaths(Node root,int arr[],int index)
+    {
+        if(root==null)
+        return;
+        arr[index]=root.data;
+        if(root.left==null&&root.right==null)
+        {
+           for(int i=0;i<=index;i++)
+           System.out.print(arr[i]+" ");
+           System.out.println();
+
+        }
+        findallpaths(root.left,arr,index+1);
+        findallpaths(root.right,arr,index+1);
+       
+    }
     public static void main(String args[])
     {
         binarysearchtree bs=new binarysearchtree();
@@ -108,10 +200,15 @@ public class binarysearchtree {
         root.left=new Node(20);
         root.right=new Node(500);
         bs.insert(root,1);
+        bs.insert(root,0);
+        bs.insert(root,2);
         bs.insert(root,30);
         bs.insert(root,900);
-        bs.delete(100);
+ 
         
         bs.levelordertraversal(root);
+        bs.minimummaximum(root);
+        System.out.println();
+        
     }
 }
